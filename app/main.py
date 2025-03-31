65413b828bc9a66ebfc7b7d2469a1b513d4de053
22f3002229 <22f3002229@ds.study.iitm.ac.in>
2025-03-31 14:19:05 +0530

    third

--- app/main.py
+++ app/main.py
@@ -17,7 +17,3 @@ app.include_router(question_router)
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the FastAPI project!"}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
